37eada4b63c7e4c2bc4146370782ad9b60a5646d
julimorozova <yuli.sleptsova@gmail.com>
2021-12-14 20:15:26 +0300

    create a simple server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // index.js
 // This is the main entry point of our application
+// index.js
+// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+
+// запускаем сервер на порте, указанном в файле .env, или на порте 7000
+const port = process.env.PORT || 7000;
+
+app.get('/', (reg, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+
